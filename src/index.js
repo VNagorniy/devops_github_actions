b7395fc8f262e532e0eeb34a44e075c4bcdcd4ba
VNagorniy <vnagorniyrnd@gmail.com>
2025-12-22 15:52:56 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 4545;
+
+app.get('/ping', (req, res) => {
+	res.send({ message: 'pong' });
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on http://localhost:${PORT}`);
+});
+
+export default app;
+tests / ping.test.js;
